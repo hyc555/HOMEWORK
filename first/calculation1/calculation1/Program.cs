aa18b4587f3b51fd312da19134d8e9205f51e122
hyc555 <sixteennights@126.com>
2021-03-04 21:16:44 +0800

    first

---
+++ first/calculation1/calculation1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace calculation1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input the first number");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Input the secong number");
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Input the operator" );
+            char c = (char)Console.Read();
+            double d;
+            if (c.Equals('+'))
+            {
+                d = a + b;
+                Console.WriteLine("Result is" + d);
+            }
+            else if (c.Equals('-'))
+            {
+                d = a - b;
+                Console.WriteLine("Result is" + d);
+            }
+            else if (c.Equals('*'))
+            {
+                d = a * b;
+                Console.WriteLine("Result is" + d);
+
+            }
+            else if (c.Equals('/'))
+            {
+                d = a / b;
+                Console.WriteLine("Result is"+d);
+            }
+            else
+            {
+                Console.WriteLine("Error!");
+            }
+        }
+    }
+}
