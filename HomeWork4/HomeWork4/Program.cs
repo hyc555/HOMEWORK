b8f6234df58c77a2f9a91ee7d48a78e77b48c94e
hyc555 <sixteennights@126.com>
2021-03-25 19:55:49 +0800

    update

---
+++ HomeWork4/HomeWork4/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace HomeWork4
+{
+    class Program
+    {
+        public class Node<T>
+        {
+            public Node<T> Next { get; set; }
+            public T Data { get; set; }
+            public Node(T t) {
+                Next = null;
+                Data = t;
+            }
+        }
+        public class GenericList<T>
+        {
+            private Node<T> head;
+            private Node<T> tail;
+            public Node<T> Head { get { return head; } }
+            public GenericList()
+            {
+                head = tail = null;
+            }
+            public void Add(T t) 
+            {
+                Node<T> node = new Node<T>(t);
+                if (tail == null)
+                {
+                    head = tail = node;
+                }
+                else
+               {
+                    tail.Next = node;
+                    tail = node;
+                }
+            }
+        }
+        delegate  int Func(int x);
+        static int ForEachList(GenericList<int> t, Func func)
+        {
+            int n = 0;
+            if (t.Head != null)
+            {
+                while (t.Head.Next != null)
+                {
+                    func(t.Head.Data);
+                    t.Head.Next = t.Head;
+                }
+                return n;
+            }
+            else {
+                throw new Exception("空指针异常");
+            }
+        }
+        static void Main(string[] args)
+        {
+            GenericList<int> g = new GenericList<int>();
+            for(int i = 0; i < 5; i++)
+            {
+                g.Add(i);
+            }
+            int sum = 0;
+            ForEachList(g, x => sum += x);
+            int?max = 0;
+            ForEachList(g, x => { if (max == null) { max = x; } else { max = max > x ? max : x; } return (int)max; });
+            int?min = 0;
+            ForEachList(g, x => { if (min == null) { min = x; } else { min = min > x ? min : x; } return (int)min; });
+        }
+    }
+}
---
+++ HomeWork4/HomeWork44/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Threading;
+
+namespace HomeWork44
+{
+    class Clock
+    {
+        Time time = new Time();
+        Time alerm = new Time();
+
+        public void Run(Object state)
+        {
+            bool flagMinute = false;
+            bool flagSecond = false;
+            time.Second += 1;
+
+            if (time.Second >= 60)
+            {
+                flagSecond = true;
+            }
+            time.Second = time.Second % 60;
+            if (flagSecond == true)
+            {
+                time.Minute += 1;
+                if (time.Minute>= 60)
+                {
+                    flagMinute = true;
+                }
+                time.Minute = time.Minute % 60;
+            }
+            if (flagMinute == true)
+            {
+                time.Hour += 1;
+                time.Hour = time.Hour % 24;
+            }
+            if (time.Hour==alerm.Hour&& time.Minute==alerm.Minute && time.Second==alerm.Second )
+            {
+                DiDiDi();
+            }
+            //Console.WriteLine($"{time.Hour}+{time.Minute}+{time.Second}");
+        }
+        
+        public void SetClock(int a,int b,int c)
+        {
+            time.SetTime(a, b, c);
+        }
+        public void SetAlerm(int a, int b, int c)
+        {
+            alerm.SetTime(a, b, c);
+        }
+        public void DiDiDi()
+        {
+            Console.WriteLine("嘀嘀嘀");
+        }
+
+    }
+
+    
+    class Time
+    {
+        private int hour=0;
+        private int minute=0;
+        private int second=0;
+        public int Hour { get=>hour; set=>hour=value; }
+        public int Minute { get => minute; set => minute = value; }
+        public int Second { get => second; set => second = value; }
+        public void SetTime(int hour,int minute,int second)
+        {
+            this.hour = hour;
+            this.minute = minute;
+            this.second = second;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Clock clock = new Clock();
+            clock.SetAlerm(0, 0, 10);
+            //Thread thread = new Thread(new ThreadStart(clock.Run));
+            //thread.Start();
+            //while (true)
+            //{
+            //    Thread.Sleep(1000);
+            //}
+            System.Threading.Timer threadTimer = new System.Threading.Timer(new System.Threading.TimerCallback(clock.Run), null, 0, 1000);
+        while (true)
+        {
+            Thread.Sleep(1000);
+        }
+        }
+    }
+}
